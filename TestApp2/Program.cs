271c6f8bd1c6bdb50403fc683f23c65b2b075da3
Rinat <rinat98kgg@gmail.com>
2020-09-29 14:35:44 +0600

    Задание выполнено

--- TestApp2/Program.cs
+++ TestApp2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace TestApp2
 {
@@ -12,7 +13,9 @@ namespace TestApp2
         {
             string resultText = "";
             Encoder encoder = new Encoder();
-            resultText =  encoder.EncodeString("abc");
+            resultText =  encoder.EncodeString("abc", false);
+            Console.WriteLine(resultText);
+            resultText = encoder.EncodeString("abc", true);
             Console.WriteLine(resultText);
             Console.ReadLine();
         }
@@ -20,14 +23,28 @@ namespace TestApp2
 
     public class Encoder
     {
-        public string EncodeString(string str)
+        public string EncodeString(string str, bool isHash) //Добавил аргумент в фунцию которая будет задавать, кодировать через SHA1 или просто обычным
         {
             string res = "";
-            foreach (char ch in str)
+            if (!isHash) //Проверка значения аргумента
             {
-                char s = ch;
-                s++;
-                res += s;
+                //Этот блок выполняется если кодировка не через SHA1
+                foreach (char ch in str)
+                {
+                    char s = ch;
+                    s++;
+                    res += s;
+                }
+            }
+            else
+            {
+                //Этот блок выполняется если кодировка через SHA1
+                using (SHA1 sha1Hash = SHA1.Create())
+                {
+                    byte[] sourceBytes = Encoding.UTF8.GetBytes(str);
+                    byte[] hashBytes = sha1Hash.ComputeHash(sourceBytes);
+                    res = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+                }
             }
             return res;
         }
